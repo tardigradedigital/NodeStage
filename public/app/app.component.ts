f38cb3c0cdce769c18f6b8f2ff90ebd289d80c0f
Tech Justin <TechJustin@Coboco.local>
2016-05-11 16:18:56 -0400

    Initial commit of Auth0 integration

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,7 +1,43 @@
-import { Component } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser';
+import { Component, View, provide } from '@angular/core';
+import { RouteConfig, Router, APP_BASE_HREF, ROUTER_PROVIDERS, ROUTER_DIRECTIVES, CanActivate } from '@angular/router';
+import { HTTP_PROVIDERS, Http } from '@angular/http';
+import { AuthHttp, tokenNotExpired } from 'angular2-jwt';
 
 @Component({
-  selector: 'td-phpstage',
-  template: '<h2>Tardigrade PHP Stage</h2>'
+  selector: 'td-nodestage',
+  template: `
+    <h2>Tardigrade Node Stage</h2>
+    <button *ngIf="!loggedIn()" (click)="login()">Login</button>
+    <button *ngIf="loggedIn()" (click)="logout()">Logout</button>
+  `
 })
-export class AppComponent {}
+export class AppComponent {
+  lock = new Auth0Lock('jRop2sapEBB46vgXAuTWkYZGIvoGCQVp', 'tardigrade.auth0.com');
+  
+  constructor() {}
+  
+  login() {
+    var hash = this.lock.parseHash();
+    if(hash) {
+      if(hash.error) console.log('There was an error logging in', hash.error);
+      else this.lock.getProfile(hash.id_token, function(err, profile) {
+        if(err) {
+          console.log(err);
+          return;
+        }
+        localStorage.setItem('profile', JSON.stringify(profile));
+        localStorage.setItem('id_token', hash.id_token);
+      });
+    }
+  }
+  
+  logout() {
+    localStorage.removeItem('profile');
+    localStorage.removeItem('id_token');
+  }
+  
+  loggedIn() {
+    return tokenNotExpired();
+  }
+}
--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -5,7 +5,8 @@
     'app':                        'app', // 'dist',
     'rxjs':                       'node_modules/rxjs',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    '@angular':                   'node_modules/@angular'
+    '@angular':                   'node_modules/@angular',
+    'angular2-jwt':               'node_modules/angular2-jwt/angular2-jwt'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -13,6 +14,7 @@
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    'angular2-jwt':               { defaultExtension: 'js'} 
   };
 
   var packageNames = [
@@ -34,6 +36,7 @@
   });
 
   var config = {
+    defaultJSExtension: true,
     map: map,
     packages: packages
   }
