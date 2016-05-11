566685450a71cc8a731e2d963e479dc31d4bed36
Tech Justin <TechJustin@Coboco.local>
2016-05-11 12:00:32 -0400

    Create environment for Node

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-var http = require('http');
-http.createServer(function(req, res) {
-    console.log('Request: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h3>Tardigrade Node.JS Stage is running</h3>');
-}).listen(process.env.PORT);
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uw03mypu');
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
+app.get('*', function(req, res) {
+    res.sendfile('./public/index.html');
+})
+
+app.listen(80);
+console.log("Stage started on port 80");
