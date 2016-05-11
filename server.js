4c73ad96be422d2904be28cba563e1fe8633ab5f
Tech Justin <TechJustin@Coboco.local>
2016-05-11 11:14:19 -0400

    Test commit

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function(req, res) {
     console.log('Request: ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h3>Tardigrade Node.JS Stage is running</h3>');
+    res.end('<h3>Tardigrade Node.Js Stage is running</h3>');
 }).listen(process.env.PORT);
