e2f958cf35eb23bd448780b32bfe0a403619e2fb
Justin Lindsey <Justin Lindsey>
2016-05-12 00:41:54 -0400

    Added TODO notes for next steps

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,6 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { Component, View, provide } from '@angular/core';
-import { RouteConfig, Router, APP_BASE_HREF, ROUTER_PROVIDERS, ROUTER_DIRECTIVES, CanActivate } from '@angular/router-deprecated';
+import { Component, provide } from '@angular/core'; /* Removed View from imports */
+import { RouteConfig, Router, ROUTER_PROVIDERS, ROUTER_DIRECTIVES, CanActivate } from '@angular/router-deprecated'; /* Removed APP_BASE_HREF from imports */
 import { HTTP_PROVIDERS, Http } from '@angular/http';
 import { AuthHttp, tokenNotExpired } from 'angular2-jwt';
 
@@ -8,6 +8,15 @@ import { AuthHttp, tokenNotExpired } from 'angular2-jwt';
   selector: 'td-nodestage',
   templateUrl: '/login/login.html'
 })
+
+/*
+  TODO:
+    * See if we can hide the validation error for Auth0Lock not being found
+    * Running this.login() in the constructor stops page processing and does
+      not output anything to td-nodestage. Instead of running directly in the
+      constructor, set the state as a flag and have an event call at the end
+      of page processing to trigger the login method.
+*/
 
 export class AppComponent {
   lock = new Auth0Lock('jRop2sapEBB46vgXAuTWkYZGIvoGCQVp', 'tardigrade.auth0.com');
