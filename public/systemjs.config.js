c61fd9da6c0561a2de8df5caf898ffae346ee1a5
Justin Lindsey <Justin Lindsey>
2016-05-11 21:44:56 -0400

    Updated mapping in SystemJS

--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -6,7 +6,7 @@
     'rxjs':                       'node_modules/rxjs',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     '@angular':                   'node_modules/@angular',
-    'angular2-jwt':               'node_modules/angular2-jwt/angular2-jwt'
+    'angular2-jwt':               'node_modules/angular2-jwt'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
