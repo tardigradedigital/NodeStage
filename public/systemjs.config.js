3c635cc852d1b5d001d1e84f0e3dfd5278e2d60f
Tech Justin <TechJustin@Coboco.local>
2016-05-12 09:39:37 -0400

    Updated module mappings

--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -6,7 +6,8 @@
     'rxjs':                       'node_modules/rxjs',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     '@angular':                   'node_modules/@angular',
-    'angular2-jwt':               'node_modules/angular2-jwt/angular2-jwt.js'
+    'angular2-jwt':               'node_modules/angular2-jwt/angular2-jwt.js',
+    'td-login':                   'public/login/login.js'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -14,7 +15,8 @@
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' },
-    'angular2-jwt':               { defaultExtension: 'js' } 
+    'angular2-jwt':               { defaultExtension: 'js' },
+    'td-login':                   { defaultExtension: 'js' }
   };
 
   var packageNames = [
