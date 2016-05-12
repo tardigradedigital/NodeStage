edabcaa290e1358bcf0fb0b17c22834e7ab4f662
Tech Justin <TechJustin@Coboco.local>
2016-05-12 11:01:03 -0400

    Updated login show logic

--- public/login/login.ts
+++ public/login/login.ts
@@ -7,26 +7,29 @@ export class LoginService {
   lock = new Auth0Lock('jRop2sapEBB46vgXAuTWkYZGIvoGCQVp', 'tardigrade.auth0.com');
     
   login() {
-    this.lock.show({
-      closeable: false,
-      disableResetAction: true,
-      disableSignupAction: true,
-      container: 'login',
-      icon: '/img/login.png'
-    }, (error: string, profile: Object, id_token: string) => {
-      var hash = this.lock.parseHash();
-      if(hash) {
-        if(hash.error) console.log('There was an error logging in', hash.error);
-        else this.lock.getProfile(hash.id_token, function(err, profile) {
-          if(err) {
-            console.log(err);
-            return;
-          }
-          localStorage.setItem('profile', JSON.stringify(profile));
-          localStorage.setItem('id_token', hash.id_token);
-        });
-      }
-    });
+    if(document.getElementById('login').getAttribute('data-lgld') == '0') {
+      this.lock.show({
+        closeable: false,
+        disableResetAction: true,
+        disableSignupAction: true,
+        container: 'login',
+        icon: '/img/login.png'
+      }, (error: string, profile: Object, id_token: string) => {
+        var hash = this.lock.parseHash();
+        if(hash) {
+          if(hash.error) console.log('There was an error logging in', hash.error);
+          else this.lock.getProfile(hash.id_token, function(err, profile) {
+            if(err) {
+              console.log(err);
+              return;
+            }
+            localStorage.setItem('profile', JSON.stringify(profile));
+            localStorage.setItem('id_token', hash.id_token);
+          });
+        }
+      });
+      document.getElementById('login').setAttribute('data-lgld', '1');
+    }
   }
   
   logout() {
