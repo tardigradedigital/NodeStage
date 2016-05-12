7d768b52bc6eea7ae132e78b5ae4469cee2e367f
Justin Lindsey <Justin Lindsey>
2016-05-11 22:18:07 -0400

    Reapplied change to SystemJS to allow jwt

--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -6,7 +6,7 @@
     'rxjs':                       'node_modules/rxjs',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     '@angular':                   'node_modules/@angular',
-    'angular2-jwt':               'node_modules/angular2-jwt/angular2-jwt'
+    'angular2-jwt':               'node_modules/angular2-jwt/angular2-jwt.js'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -14,7 +14,7 @@
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' },
-    '/angular2-jwt':               { defaultExtension: 'js' } 
+    'angular2-jwt':               { defaultExtension: 'js' } 
   };
 
   var packageNames = [
