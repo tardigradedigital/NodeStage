a906c4799bbd5fc15dc6a4deaa5d2bd1e4e672dd
Justin Lindsey <Justin Lindsey>
2016-05-14 23:29:04 -0400

    Updated styles

--- public/js/site.js
+++ public/js/site.js
@@ -1,15 +1,36 @@
 (function() {
+  break;
+  var il = [];
+  var ic = 5;
+  
+  for(i = 1; i <= ic; i++) {
+    var img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = function() {
+      var c = document.createElement('CANVAS');
+      var ctx = c.getContext('2d');
+      var data;
+      c.height = this.height;
+      c.width = this.width;
+      ctx.drawImage(this, 0, 0);
+      data = c.toDataURL();
+      il.push(data);
+      c = null
+    }
+    img.src = '/img/background-' + i + '.jpg';
+    img.remove();
+  }
+  
   function stageBg(last) {
     var img = [1, 2, 3, 4, 5];
     var rndm = img[Math.floor(Math.random() * img.length)];
-    if(rndm == last) stageBg(last);
-    $('body').animate({backgroundColor: 'rgba(35,35,35,1)'}, 200)
-    $('<img/>').attr('src', '/img/background-' + rndm + '.jpg').load(function() {
-      $(this).remove();
-      $('html').css({'background-image': 'url(/img/background-' + rndm + '.jpg)'});
+    if(rndm === last) { stageBg(last); }
+    else {
+      $('body').animate({backgroundColor: '#242424'}, 200)
+      $('html').css({'background-image': il[rndm]});
       setTimeout(function() {$('body').animate({backgroundColor: 'rgba(35,35,35,0.5)'}, 500);}, 400);
-    });
-    setInterval(stageBg, 5000, rndm);
+      setTimeout(stageBg, 15000, rndm);
+    }
   }
-  stageBg();
+  //stageBg();
 })();
