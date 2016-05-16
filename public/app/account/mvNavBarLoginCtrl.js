3cd39080a6f3bd4e7958cc9815f29a02f1efd11e
Tech Justin <TechJustin@Coboco.local>
2016-05-16 09:18:11 -0400

    Updated authentication logic

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,9 +1,18 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth, $location) {
   $scope.identity = mvIdentity;
   $scope.signin = function(username, password) {
     mvAuth.authenticateUser(username, password).then(function(success) {
       if(success) { mvNotifier.notify('You have been successfully authenticated.') }
       else { mvNotifier.notify('Invalid credentials were specified') }
     });
+  }
+  
+  $scope.signout = function() {
+    mvAuth.logoutUser().then(function() {
+      $scope.username = "";
+      $scope.password = "";
+      mvNotifier.notify('You have been signed out');
+      $location.path('/');
+    })
   }
 })
