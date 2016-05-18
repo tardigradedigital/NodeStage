c8b1847cdcef02df2663a5ef0a8ad862abf2fa18
Tech Justin <TechJustin@Coboco.local>
2016-05-18 15:08:00 -0400

    Updated Javascript to allow dropdown menu to close on outside click

--- public/js/site.js
+++ public/js/site.js
@@ -1,35 +1,43 @@
-(function() {
-  var il = [];
-  var ic = 5;
+$(document).ready(function () {
+  $(document).click(function (event) {
+    var clickover = $(event.target);
+    var _opened = $(".navbar-collapse").hasClass("navbar-collapse in");
+    if (_opened === true && !clickover.hasClass("navbar-toggle")) { $("button.navbar-toggle").click(); }
+  });
+});
+
+// (function() {
+//   var il = [];
+//   var ic = 5;
   
-  for(i = 1; i <= ic; i++) {
-    var img = new Image();
-    img.crossOrigin = 'Anonymous';
-    img.onload = function() {
-      var c = document.createElement('CANVAS');
-      var ctx = c.getContext('2d');
-      var data;
-      c.height = this.height;
-      c.width = this.width;
-      ctx.drawImage(this, 0, 0);
-      data = c.toDataURL();
-      il.push(data);
-      c = null
-    }
-    img.src = '/img/background-' + i + '.jpg';
-    img.remove();
-  }
+//   for(i = 1; i <= ic; i++) {
+//     var img = new Image();
+//     img.crossOrigin = 'Anonymous';
+//     img.onload = function() {
+//       var c = document.createElement('CANVAS');
+//       var ctx = c.getContext('2d');
+//       var data;
+//       c.height = this.height;
+//       c.width = this.width;
+//       ctx.drawImage(this, 0, 0);
+//       data = c.toDataURL();
+//       il.push(data);
+//       c = null
+//     }
+//     img.src = '/img/background-' + i + '.jpg';
+//     img.remove();
+//   }
   
-  function stageBg(last) {
-    var img = [1, 2, 3, 4, 5];
-    var rndm = img[Math.floor(Math.random() * img.length)];
-    if(rndm === last) { stageBg(last); }
-    else {
-      $('body').animate({backgroundColor: '#242424'}, 200)
-      $('html').css({'background-image': il[rndm]});
-      setTimeout(function() {$('body').animate({backgroundColor: 'rgba(35,35,35,0.5)'}, 500);}, 400);
-      setTimeout(stageBg, 15000, rndm);
-    }
-  }
-  //stageBg();
-})();
+//   function stageBg(last) {
+//     var img = [1, 2, 3, 4, 5];
+//     var rndm = img[Math.floor(Math.random() * img.length)];
+//     if(rndm === last) { stageBg(last); }
+//     else {
+//       $('body').animate({backgroundColor: '#242424'}, 200)
+//       $('html').css({'background-image': il[rndm]});
+//       setTimeout(function() {$('body').animate({backgroundColor: 'rgba(35,35,35,0.5)'}, 500);}, 400);
+//       setTimeout(stageBg, 15000, rndm);
+//     }
+//   }
+//   //stageBg();
+// })();
