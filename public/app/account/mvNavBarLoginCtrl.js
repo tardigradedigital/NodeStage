f147de2c1f6f347defdf5032093c99eafc9f7814
Tech Justin <TechJustin@Coboco.local>
2016-05-25 16:54:22 -0400

    Updated login menu; working on closing ticker when clicked outside

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,13 +1,22 @@
 angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth, $location) {
   $scope.identity = mvIdentity;
   $scope.signin = function(username, password) {
-    mvAuth.authenticateUser(username, password).then(function(success) {
-      if(success) { 
-        mvNotifier.notify('You have been successfully authenticated.');
-        if($location.path('login')) { $location.path('/'); }
-      }
-      else { mvNotifier.error('Invalid credentials were specified.') }
-    });
+    console.log(username);
+    if(!username || !password || username.$invalid || password.$invalid) {
+      if(!username || username.$invalid) { $(navLogin.username).addClass('invalid'); }
+      if(!password || password.$invalid) { $(navLogin.password).addClass('invalid'); }
+      mvNotifier.error('Please enter a valid email address and password.');
+      return false;  
+    }
+    else {
+      mvAuth.authenticateUser(username, password).then(function(success) {
+        if(success) { 
+          mvNotifier.notify('You have been successfully authenticated.');
+          if($location.path('login')) { $location.path('/'); }
+        }
+        else { mvNotifier.error('Invalid email address or password was given.') }
+      });      
+    }
   }
   
   $scope.signout = function() {
--- public/app/common/mvTickerCtrl.js
+++ public/app/common/mvTickerCtrl.js
@@ -2,6 +2,7 @@ angular.module('app').controller('mvTickerCtrl', function($scope, mvNotifier) {
   $scope.custom = mvNotifier.custom();
   $scope.reset = mvNotifier.reset();
   $scope.showAll = function() {
-    alert('Show Ticker');
+    console.log('Ticker toggled');
+    $('.ticker-container').toggleClass('expanded');
   }
 });
